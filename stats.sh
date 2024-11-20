5b4bf7a09763ece16bd8e6630fbefec29018c923
Sam <samgolovchenko@gmail.com>
2024-11-20 13:16:39 +0300

    update fetch info logic

--- stats.sh
+++ stats.sh
@@ -20,7 +20,8 @@ if [[ -z "$validator_data" ]]; then
 fi
 
 # Sorting and removing the last 1000 validators from consideration
-sorted_validator_data=$(echo "$validator_data" | jq '[.validators[]] | sort_by(.epochCredits) | reverse | .[0:-1000]')
+sorted_validator_data=$(echo "$validator_data" | jq '[.validators[]] | sort_by(.epochCredits) | reverse')
+sorted_validator_data_trimmed=$(echo "$validator_data" | jq '[.validators[]] | sort_by(.epochCredits) | reverse | .[0:-1000]')
 
 epoch_info=$(solana epoch-info --output json)
 epoch=$(echo "$epoch_info" | jq -r '.epoch')
@@ -30,12 +31,12 @@ epoch_completed_percent=$(printf "%.1f" "$epoch_completed_percent")
 previous_epoch=$((epoch - 1))
 next_epoch=$((epoch + 1))
 
-average_credits=$(echo "$sorted_validator_data" | jq -r '[.[] | select(.epochCredits > 0 and .activatedStake > 0 and .skipRate != null) | .epochCredits] | add / length')
+average_credits=$(echo "$sorted_validator_data_trimmed" | jq -r '[.[] | select(.epochCredits > 0 and .activatedStake > 0 and .skipRate != null) | .epochCredits] | add / length')
 average_credits=$(printf "%.0f" $average_credits)
 
 validator_info=$(echo "$sorted_validator_data" | jq -r --arg VALIDATOR_IDENTITY "$VALIDATOR_IDENTITY" '.[] | select(.identityPubkey == $VALIDATOR_IDENTITY)')
 if [[ -z "$validator_info" ]]; then
-  echo "Error: Failed to fetch validator info."
+  echo "Error: Failed to fetch validator info. Identity - $VALIDATOR_IDENTITY"
   exit 1
 fi
 
@@ -46,12 +47,14 @@ validator_commission=$(echo "$validator_info" | jq -r '.commission')
 validator_stake=$(printf "%.0f" $(bc <<< "scale=0; $validator_stake / 1000000000"))
 validator_place=$(echo "$sorted_validator_data" | jq -r --arg VALIDATOR_ID "$VALIDATOR_IDENTITY" 'to_entries[] | select(.value.identityPubkey == $VALIDATOR_ID) | (.key + 1)')
 
+total_validators=$(echo "$sorted_validator_data" | jq -r 'length')
+
 leader_info=$(echo "$sorted_validator_data" | jq -r '.[0]')
 leader_credits=$(echo "$leader_info" | jq -r '.epochCredits')
 leader_pubkey=$(echo "$leader_info" | jq -r '.identityPubkey')
 diff_leader=$((validator_credits - leader_credits))
 
-cluster_version=$(echo "$sorted_validator_data" | jq -r '[group_by(.version) | max_by(length)][0][0].version')
+cluster_version=$(echo "$sorted_validator_data_trimmed" | jq -r '[group_by(.version) | max_by(length)][0][0].version')
 
 leader_slots=$(solana leader-schedule --epoch "$epoch" | grep "$VALIDATOR_IDENTITY" | wc -l)
 previous_leader_slots=$(solana leader-schedule --epoch "$previous_epoch" | grep "$VALIDATOR_IDENTITY" | wc -l)
@@ -108,7 +111,7 @@ printf "%-20s %-20s\n" "Leader Slots  " "$(colorize_leader_slots)"
 printf "%-20s %-20s\n" "Previous epoch" "$previous_leader_slots"
 printf "%-20s %-20s\n" "Next epoch    " "$(colorize_next_epoch_slots)"
 echo ""
-printf "%-20s %-20s\n" "Place" "$validator_place"
+printf "%-20s %-20s\n" "Place" "$validator_place" "$total_validators"
 printf "%-20s %-20s\n" "Stake" "$(colorize_stake)"
 printf "%-20s %-20s\n" "Commission" "$validator_commission%"
 printf "%-20s %-20s\n" "Version" "$validator_version (Cluster: $cluster_version)"
